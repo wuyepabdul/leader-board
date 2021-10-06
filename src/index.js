e6ad2bef7129aed6e8affa68d879047b42054490
wuyepabdul <wuyepabdul@gmail.com>
2021-10-06 12:46:27 +0100

    Add alert div

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+
+const createGame = () => {
+  let value;
+  const newGame = document.querySelector('#game');
+  const createBtn = document.querySelector('#new-game-btn');
+  newGame.addEventListener('change', (e) => {
+    value = e.target.value;
+  });
+
+  createBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('clicked');
+    console.log('value', value);
+  });
+};
+
+createGame();
